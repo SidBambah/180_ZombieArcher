12c43d1d6f0f66e25932f408ec5fe1c1fed23ec6
Sidharth Bambah <sids53@gmail.com>
2018-11-29 22:35:58 -0800

    Added updated game

---
+++ Unity Game/Assets/EnemyManager.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Description: 
+// Every 7 seconds, an attempt to spawn a zombie occurs
+// If the player is dead or there are 3 zombies, then a zombie does not spawn
+// Otherwise, a zombie is spawned randomly out of 3 positions, and in a location 
+// where there is not a zombie
+
+public class EnemyManager : MonoBehaviour {
+
+    // Public variables
+    public PlayerHealth playerHealth; // Reference to player's health
+    public GameObject enemy;          // Reference to enemy to make duplicate of
+    public float spawnTime = 10f;      // How often to spawn enemies
+    public Transform[] spawnPoints;   // Location to spawn enemies
+
+
+    // Static variables
+    public static int numberZombies;        // Number of zombies in scene
+    public static bool[] positionTaken;   // Element is true if that position is taken by zombie
+
+    // Private variables
+    private int spawnPointIndex;    //Indexes into spawnPoint array 
+    private int maxZombies = 3;
+
+
+	// Use this for initialization
+	void Start () {
+        // Calls Spawn function every spawnTime seconds 
+        // starting at time spawnTime
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+
+        // Initially, one zombie in scene
+        numberZombies = 0;
+
+        // For now, we will have 3 spawn points
+        positionTaken = new bool[maxZombies];
+
+        // Indicate that there are no zombies occupying positions
+        for (int k = 0; k < maxZombies; k++)
+        {
+            positionTaken[k] = false;
+        }
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Spawn()
+    {
+        // If the player is dead
+        if (playerHealth.currentHealth <= 0f)
+        {
+            return;
+        }
+
+        // If there are 3 zombies in the scene
+        if (numberZombies == maxZombies)
+        {
+            return;
+        }
+        //spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        // Get random index to a spawn point where there is currently not a zombie
+        while (true)
+        {
+            spawnPointIndex = Random.Range(0, spawnPoints.Length);
+            if (positionTaken[spawnPointIndex] == false)
+            {
+                break;
+            }
+        }
+
+        GameObject newZombie;
+
+        // Instantiate zombie in spawnpoint
+        newZombie = Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+
+        // Give zombie its unique spawn point index
+        newZombie.GetComponent<ZombieHealth>().positionTakenIndex = spawnPointIndex;
+
+        // Indicate the position is now occupied
+        positionTaken[spawnPointIndex] = true;
+
+        // Increment number of zombies in scene
+        numberZombies += 1;
+    }
+
+
+}
---
+++ Unity Game/Assets/TutorialController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Description: 
+// 
+public class TutorialController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// Start in Stage1, i.e. call function Stage1
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        // If you get a pause signal, pause the game
+	}
+
+
+
+
+
+
+    void Stage1() 
+    {
+        // Spawn zombie one at a time at varying (increasing) distances
+
+        // Close zombie: 
+        // Unlimited tries
+
+        // Next zombie: 
+        // 5 tries
+        // If you fail to kill the zombie, destroy current zombie 
+        // and spawn close zombie again
+
+
+        // Far zombie: 
+        // 5 tries
+        // If you fail to kill the zombie, destroy current zombie
+        // and spawn middle zombie
+
+        // If you successfully kill all zombies, call Stage2 function
+
+
+
+    }
+
+
+    void Stage2()
+    {
+        // Same logic as Stage1, except zombies spawned horizontally
+
+    }
+
+    void Stage3()
+    {
+        // Ducking under flying object
+    }
+
+    void Stage4()
+    {
+        // Random motion of zombies
+        // Move zombies in a circle
+    }
+
+
+    // Create method to destroy all zombies in scene
+
+    // For pause method, get reference to zombies
+    // Disable movement for each of them
+    // In update method, continuosly listen for 
+
+
+
+
+
+
+
+}
